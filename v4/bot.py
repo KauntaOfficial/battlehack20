de6c2a66672a24a0fa95691cf344823ae7421ef5
Ivy Zhang <ZIvy042003@gmail.com>
2020-04-19 13:33:24 -0400

    v4:     - Added priority lanes where there are uncontested pawns "running it down mid" xd

--- v4/bot.py
+++ v4/bot.py
@@ -137,6 +137,8 @@ def turn():
         else:
             forward = -1
 
+        # If a lane is uncontested, clog it up
+        priority_lane = -1
         # First, pick a decent enough col to start off with
         col_to_place = -1
         for col in range(0, board_size):
@@ -182,18 +184,22 @@ def turn():
                 elif check_space(row, col) == team:
                     your_count += 1
 
-            # if 0 < col < 15:
-            #     if check_space(opp, col - 1) == team and check_space(opp, col + 1) == team:
-            #         if your_count == 0:
-            #             col_to_place = col
-            #             break
-            #         else:
-            #             continue
+            priority = True
+            for row in range(1, 15):
+                if check_space(row, col) == team:
+                    priority = False
+                elif check_space(row, col) == opp_team:
+                    break
+
+            if priority and not check_space(vert, col):
+                priority_lane = col
+                break
 
             # If you're losing harder, then take it
             if your_count - opp_count <= diff and not check_space(vert, col):
                 col_to_place = col
                 diff = your_count - opp_count
+            priority_lane = 1
 
         # If chosen spot is eaten, then you want to go to your last resort which should be behind a won lane
         if check_space_wrapper(vert + forward, col_to_place - 1, board_size) == opp_team or check_space_wrapper(
@@ -222,9 +228,9 @@ def turn():
                 col_to_place = test_col
                 min_in_row = in_row
 
-
-
-        if 0 <= col_to_place <= 15 and not check_space(vert, col_to_place):
+        if priority_lane != -1:
+            spawn(vert, priority_lane)
+        elif 0 <= col_to_place <= 15 and not check_space(vert, col_to_place):
             spawn(vert, col_to_place)
 
         # for _ in range(board_size):
