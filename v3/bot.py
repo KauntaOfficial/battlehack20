5da3bc6f9749471c803c4158ae95584904fa707d
ImDaNoob <bjchappell02@icloud.com>
2020-04-19 14:28:10 -0400

    buggy v4 spawning system is bad tbh

--- v3/bot.py
+++ v3/bot.py
@@ -132,7 +132,7 @@ def turn():
 
         if lStage == 0:
             lStage = 1
-            lCenterLane = spawnLanev1(vert, opp, step, team, opp_team, board_size)
+            lCenterLane = spawnLanev4(vert, opp, step, team, opp_team, board_size)
             spawn(vert, lCenterLane)
             
         # Occurs when the first piece of the L is placed, places the second piece.
@@ -168,7 +168,7 @@ def turn():
             # if you cannot place the final piece, go back to starting another L
             else:
                 lstage = 1
-                lCenterLane = spawnLanev1(vert, opp, step, team, opp_team, board_size)
+                lCenterLane = spawnLanev4(vert, opp, step, team, opp_team, board_size)
                 spawn(vert, lCenterLane)
 
 
@@ -259,8 +259,7 @@ def spawnLanev4(vert, opp, step, team, opp_team, board_size):
     else:
         forward = -1
 
-    # If a lane is uncontested, clog it up
-    priority_lane = -1
+
     # First, pick a decent enough col to start off with
     col_to_place = -1
     for col in range(0, board_size):
@@ -286,6 +285,9 @@ def spawnLanev4(vert, opp, step, team, opp_team, board_size):
         elif check_space(row, col_to_place) == team:
             diff += 1
 
+    # If a lane is uncontested, clog it up
+    priority_lane = -1
+    # Last resort in case your lane is bad
     last_resort = -1
     for col in range(0, board_size):
         if check_space(opp, col) == team:
@@ -307,8 +309,9 @@ def spawnLanev4(vert, opp, step, team, opp_team, board_size):
                 your_count += 1
 
         # Checks to make sure there aren't any breakaway pawns
+        # Scans from your side to the other!
         priority = True
-        for row in range(1, 15):
+        for row in range(vert + forward, opp, forward):
             if check_space(row, col) == team:
                 priority = False
             elif check_space(row, col) == opp_team:
@@ -322,7 +325,6 @@ def spawnLanev4(vert, opp, step, team, opp_team, board_size):
         if your_count - opp_count <= diff and not check_space(vert, col):
             col_to_place = col
             diff = your_count - opp_count
-        priority_lane = 1
 
     # If chosen spot is eaten, then you want to go to your last resort which should be behind a won lane
     if check_space_wrapper(vert + forward, col_to_place - 1, board_size) == opp_team or check_space_wrapper(
