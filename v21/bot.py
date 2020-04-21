b417d023d8cae29374f73541c03447dcc3e596bf
ImDaNoob <bjchappell02@icloud.com>
2020-04-21 14:00:35 -0400

    v21 finished, better pawn code

--- v21/bot.py
+++ v21/bot.py
@@ -7,6 +7,10 @@ from battlehack20.stubs import *
 # when to push instead of eat, then you can defend and win. Also could be interesting strategy to
 # try and sneak some pawns down the side when the middle is locked up.
 
+# Only changes made were to the spawning. Changed the first priority spawn to be the lane in which the enemy is closest.
+# This is v13 but with v15 pawns    
+# This is v18 but with some small mirco enhancements.
+
 DEBUG = 0
 
 
@@ -57,19 +61,26 @@ def turn():
         #    dlog('Pushed Forward')
         # try capturing pieces
 
+        captureLeft = False
+        captureRight = False
+        prioritizeLeft = False
+        prioritizeRight = False
         reinforce = False
         push = True
         opponents = 0
         allies = 0
+
+        # Ultra defensive, make sure you have both supports
+        if check_space_wrapper(row + (forward * -1), col + 1, board_size) and check_space_wrapper(row + (forward * -1), col + 1, board_size) != team:
+            push = False
+        if check_space_wrapper(row + (forward * -1), col - 1, board_size) and check_space_wrapper(row + (forward * -1), col - 1, board_size) != team:
+            push = False
+
         # Check to see if you can break favorably
         if check_space_wrapper(row + (forward * 2), col + 1, board_size) == opp_team:
             opponents += 1
-            if check_space_wrapper(row + (forward * -1), col + 1, board_size) != team:
-                push = False
         if check_space_wrapper(row + (forward * 2), col - 1, board_size) == opp_team:
             opponents += 1
-            if check_space_wrapper(row + (forward * -1), col - 1, board_size) != team:
-                push = False
         if check_space_wrapper(row, col + 1, board_size) == team:
             allies += 1
         if check_space_wrapper(row, col - 1, board_size) == team:
@@ -81,16 +92,15 @@ def turn():
             push = True
 
         # if push:
-        # dlog("PUSH")
+            # dlog("PUSH")
         # else:
-        # dlog("NOPUSH")
+            # dlog("NOPUSH")
+
+        
+
 
         # Make sure you aren't the next in a pawn chain that has to move.
-        # TODO: Make sure you're reinforcing the first pawn in a line WHILE not being taken
-        # TODO: Make sure if you aren't crucial reinforcement, PUSH PUSH PUSH
-        if check_space_wrapper(row + forward, col + 1, board_size) == team and check_space_wrapper(row + (forward * 2),
-                                                                                                   col + 2,
-                                                                                                   board_size) == opp_team:  # up and right
+        if check_space_wrapper(row + forward, col + 1, board_size) == team and check_space_wrapper(row + (forward * 2),col + 2,board_size) == opp_team:  # up and right
             reinforce = True
         elif check_space_wrapper(row + forward, col - 1, board_size) == team and check_space_wrapper(
                 row + (forward * 2), col - 2, board_size) == opp_team:  # up and left
@@ -111,20 +121,66 @@ def turn():
             reinforce = False
 
         if check_space_wrapper(row + forward, col + 1, board_size) == opp_team:  # up and right
-            capture(row + forward, col + 1)
+            captureRight = True
             # dlog('Captured at: (' + str(row + forward) + ', ' + str(col + 1) + ')')
 
         elif check_space_wrapper(row + forward, col - 1, board_size) == opp_team:  # up and left
-            capture(row + forward, col - 1)
+            captureLeft = True
             # dlog('Captured at: (' + str(row + forward) + ', ' + str(col - 1) + ')')
+
+
+        #Check for the arrow pattern, meaning that you should not capture.
+        if (check_space_wrapper(row+forward, col, board_size) == opp_team and # Check right in front for an opponent
+            check_space_wrapper(row, col+1, board_size) == opp_team and # Check to the right for an opponent
+            check_space_wrapper(row, col-1, board_size) == opp_team and # Check to the left for an opponent
+            check_space_wrapper(row+(forward*-1), col+1, board_size) == team and # Check one back and to the right for a friendly
+            check_space_wrapper(row+(forward*-1), col-1, board_size) == team): # Check one back and to the left for a friendly
+            captureLeft = False
+            captureRight = False
+
+        # Choose which capture to prioritize, left or right, based on whether pawns are ready to trade on either side.
+        rightTrades = 0
+        leftTrades = 0
+        # Get all the pieces ready to trade to the right
+        if check_space_wrapper(row, col+2, board_size) == team: # Check 2 spaces to the right for a friendly.
+            rightTrades += 1
+            if check_space_wrapper(row+(forward*-1), col+2, board_size) == team: # Check two to the right and one back for a friendly
+                rightTrades += 1
+                if check_space_wrapper(row+(forward*-2), col+2, board_size) == team: # Check two to the right and two back for a friendly
+                    rightTrades += 1
+        # Get all the pieces ready to trade to the left
+        if check_space_wrapper(row, col-2, board_size) == team: # Check 2 spaces to the left for a friendly.
+            leftTrades += 1
+            if check_space_wrapper(row+(forward*-1), col-2, board_size) == team: # Check two to the left and one back for a friendly
+                leftTrades += 1
+                if check_space_wrapper(row+(forward*-2), col-2, board_size) == team: # Check two to the left and two back for a friendly
+                    leftTrades += 1
+
+        if rightTrades > leftTrades:
+            prioritizeRight = True
+        elif leftTrades > rightTrades:
+            prioritizeLeft = True
+        else:
+            prioritizeLeft = prioritizeRight = False
+            
+
+        if prioritizeLeft and captureLeft and not prioritizeRight:
+            capture(row+forward, col-1) 
+        elif prioritizeRight and captureRight and not prioritizeLeft:
+            capture(row+forward, col+1)
+        elif captureLeft:
+            capture(row+forward, col-1) 
+        elif captureRight:
+            capture(row+forward, col+1)
 
         # otherwise try to move forward
         elif not (not (row + forward != -1) or not (row + forward != board_size) or check_space_wrapper(row + forward,
                                                                                                         col,
-                                                                                                        board_size)) and not reinforce and push:
+                                                                                                        board_size))  and not reinforce and push:
             #               ^  not off the board    ^            and    ^ directly forward is empty
             move_forward()
             # dlog('Moved forward!')
+
 
     else:
         # Where do we want to spawn the pawns? Center > Edges since you cover two spaces
@@ -151,13 +207,51 @@ def turn():
 
         # First, pick a decent enough col to start off with
         col_to_place = -1
-        col_to_place_weight = -1
+        for col in range(0, board_size):
+            if not check_space(vert, col):
+                if check_space_wrapper(vert + forward, col - 1, board_size) == opp_team or check_space_wrapper(
+                        vert + forward, col + 1, board_size) == opp_team:
+                    continue
 
+                col_to_place = col
+                break
+
+        if col_to_place == -1:
+            for col in range(0, board_size):
+                if not check_space(vert, col):
+                    col_to_place = col
+                    break
+
+        # Find the difference in the column already chosen
+        diff = 0
+        for row in range(0, board_size):
+            if check_space(row, col_to_place) == opp_team:
+                diff -= 1
+            elif check_space(row, col_to_place) == team:
+                diff += 1
+
+        # Find the closest opposing pawn in each lane.
+        closestOppPawn = [board_size for i in range(0, board_size)]
+        closestOppPawnLane = -1
+        closestOppPawnDistance = board_size
+        for lane in range(0,board_size):
+            for row in range(vert, opp, forward):
+                if check_space_wrapper(row, lane, board_size) == opp_team:
+                    closestOppPawn[lane] = abs(vert - row)
+                    if abs(vert-row) < closestOppPawnDistance:
+                        closestOppPawnLane = lane
+                        closestOppPawnDistance = abs(vert-row)
+        # Prioritizes placing where your enemies are closer than further away
+        firstChoiceLane = closestOppPawnLane
+
+        # Set second choice to a placeholder just in case.
+        secondChoice = -1
         # If a lane is uncontested, clog it up
         priority_lane = -1
         # Last resort in case your lane is bad
         last_resort = -1
         for col in range(0, board_size):
+            #Sets the last resort to a lane that has already been won.
             if check_space(opp, col) == team:
                 if (col > 0 and check_space(vert + forward, col - 1) != opp_team) and (
                         col < board_size - 1 and check_space(vert + forward, col + 1) != opp_team):
@@ -166,14 +260,15 @@ def turn():
             if check_space_wrapper(vert + forward, col - 1, board_size) == opp_team or check_space_wrapper(
                     vert + forward, col + 1, board_size) == opp_team:
                 continue
-            col_weight = 0
+            opp_count = 0
+            your_count = 0
 
+            # Get the number of friendlies and opponents in each row.
             for row in range(0, board_size):
                 if check_space(row, col) == opp_team:
-                    col_weight += abs(row - opp)
+                    opp_count += 1
                 elif check_space(row, col) == team:
-                    col_weight -= abs(row - vert)
-                # TODO: Your own pawn weight?
+                    your_count += 1
 
             # Checks to make sure there aren't any breakaway pawns
             # Scans from your side to the other!
@@ -191,56 +286,55 @@ def turn():
                 # dlog("Found priority @ " + str(priority_lane))
                 break
 
-            dlog("weight of col is " + str(col_weight))
             # If you're losing harder, then take it
             # Maybe don't take edges.
-            if not check_space(vert, col):
-                dlog("comparing old: " + str(col_to_place_weight) + " vs new: " + str(col_weight))
-                if col_to_place == -1:
-                    dlog("default: " + str(col))
-                    col_to_place = col
-                    col_to_place_weight = col_weight
-                elif col_weight > col_to_place_weight:
-                    dlog("update: " + str(col))
-                    col_to_place = col
-                    col_to_place_weight = col_weight
+            # Compare the previous differences to the new differences, taking the greater of the differences.
+            if your_count - opp_count <= diff and not check_space(vert, col):
+                # This is the lane that you would want to choose second.
+                secondChoice = col
+                diff = your_count - opp_count
 
-        # If chosen spot is eaten, then you want to go to your last resort which should be behind a won lane
-        if check_space_wrapper(vert + forward, col_to_place - 1, board_size) == opp_team or check_space_wrapper(
-                vert + forward, col_to_place + 1, board_size) == opp_team:
-            if last_resort != -1:
-                col_to_place = last_resort
+        ## Go through each of the choices, testing to see if they're valid
+        if (check_space_wrapper(vert + forward, firstChoiceLane - 1, board_size) == opp_team or check_space_wrapper(
+                vert + forward, firstChoiceLane + 1, board_size) == opp_team) or check_space_wrapper(vert, firstChoiceLane, board_size) or firstChoiceLane == -1:
+            if (check_space_wrapper(vert + forward, secondChoice - 1, board_size) == opp_team or check_space_wrapper(
+                vert + forward, secondChoice + 1, board_size) == opp_team) or check_space_wrapper(vert, secondChoice, board_size) or secondChoice == -1:
+                if last_resort != -1:
+                    col_to_place = last_resort
+            else:
+                col_to_place = secondChoice
+        else:
+            col_to_place = firstChoiceLane
 
         # dlog("Chosen: " + str(col_to_place))
         # TODO: Change to also evaluate the adjacent columns because they all contrib and spreading out troops is beneficial
-        # TODO: cHange to also use same weighting?
-        # min_in_row = 0
-        # for i in range(0, board_size):
-        #     if check_space_wrapper(i, col_to_place, board_size) == team:
-        #         min_in_row += 1
+        min_in_row = 0
+        for i in range(0, board_size):
+            if check_space_wrapper(i, col_to_place, board_size) == team:
+                min_in_row += 1
 
-        # dlog("row before tests: " + str(col_to_place))
-        # for test_col in range(col_to_place - 2, col_to_place + 3):
-        #     if 0 > test_col or test_col > board_size - 1:
-        #         continue
-        #     if check_space(vert, test_col):
-        #         continue
-        #
-        #     in_row = 0
-        #     for row in range(0, board_size):
-        #         if check_space_wrapper(row, test_col, board_size) == team:
-        #             in_row += 1
-        #     if in_row < min_in_row:
-        #         col_to_place = test_col
-        #         min_in_row = in_row
+        dlog("row before tests: " + str(col_to_place))
+        for test_col in range(col_to_place - 2, col_to_place + 2):
+            if 0 > test_col or test_col > board_size - 1:
+                continue
+            if check_space(vert, test_col):
+                continue
 
-        # dlog("row after tests: " + str(col_to_place))
+            in_row = 0
+            for row in range(0, board_size):
+                if check_space_wrapper(row, test_col, board_size) == team:
+                    in_row += 1
+            if in_row <= min_in_row:
+                col_to_place = test_col
+                min_in_row = in_row
+
+        dlog("row after tests: " + str(col_to_place))
         # If you find a priority lane with uncontested pawns, challenge it.
         if priority_lane != -1:
-            dlog("Chose Priority: " + str(priority_lane))
+            # dlog("Chose Priority: " + str(priority_lane))
             spawn(vert, priority_lane)
         elif 0 <= col_to_place <= board_size - 1 and not check_space(vert, col_to_place):
-            dlog("Chose: " + str(col_to_place))
+            # dlog("Chose: " + str(col_to_place))
             spawn(vert, col_to_place)
 
         # for _ in range(board_size):
