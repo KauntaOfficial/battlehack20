4202f63adced50477efc1549477cd20f02c63f2b
ImDaNoob <bjchappell02@icloud.com>
2020-04-22 12:21:34 -0400

    v29 chooses the lane with the least pawns around the greatest threat lane to spawn in

---
+++ playerControlled/bot.py
@@ -0,0 +1,155 @@
+import random
+
+from battlehack20.stubs import *
+
+# Strategy
+# Note that an alternate condition if you can't win by N / 2, is just number of pawns. If you can greedy and decide
+# when to push instead of eat, then you can defend and win. Also could be interesting strategy to
+# try and sneak some pawns down the side when the middle is locked up.
+
+# This version of the bot uses the pawn code from version 20
+# Uses the most up to date spawning as of version 26
+
+DEBUG = 0
+
+
+def dlog(str):
+    if DEBUG > 0:
+        log(str)
+
+
+def check_space_wrapper(r, c, board_size):
+    # check space, except doesn't hit you with game errors
+    if r < 0 or c < 0 or c >= board_size or r >= board_size:
+        return False
+    try:
+        return check_space(r, c)
+    except:
+        return None
+
+
+def turn():
+    """
+    MUST be defined for robot to run
+    This function will be called at the beginning of every turn and should contain the bulk of your robot commands
+    """
+    # dlog('Starting Turn!')
+    board_size = get_board_size()
+
+    team = get_team()
+    opp_team = Team.WHITE if team == Team.BLACK else team.BLACK
+    # dlog('Team: ' + str(team))
+
+    robottype = get_type()
+    # dlog('Type: ' + str(robottype))
+
+    if robottype == RobotType.PAWN:
+        row, col = get_location()
+        # dlog('My location is: ' + str(row) + ' ' + str(col))
+
+        if team == Team.WHITE:
+            forward = 1
+        else:
+            forward = -1
+
+        # Pawn always wants to capture unless it has can establish control through pushing forward
+        # The main strategy here will just be when is not taking a better idea.
+
+        # if not row % 2 and not check_space_wrapper(row + (forward * 2), col + 1, board_size) and not check_space_wrapper(row + (forward * 2), col - 1, board_size) and row + forward != -1 and row + forward != board_size and not check_space_wrapper(row + forward, col, board_size):
+        #    move_forward()
+        #    dlog('Pushed Forward')
+        # try capturing pieces
+
+        reinforce = False
+        push = True
+        opponents = 0
+        allies = 0
+        # Check to see if you can break favorably
+        if check_space_wrapper(row + (forward * 2), col + 1, board_size) == opp_team:
+            opponents += 1
+            if check_space_wrapper(row + (forward * -1), col + 1, board_size) != team:
+                push = False
+        if check_space_wrapper(row + (forward * 2), col - 1, board_size) == opp_team:
+            opponents += 1
+            if check_space_wrapper(row + (forward * -1), col - 1, board_size) != team:
+                push = False
+        if check_space_wrapper(row, col + 1, board_size) == team:
+            allies += 1
+        if check_space_wrapper(row, col - 1, board_size) == team:
+            allies += 1
+        # Make sure the pawns move in the beginning/whenever possible
+        if opponents == 2 or allies <= opponents:
+            push = False
+        if opponents == 0:
+            push = True
+
+        # if push:
+        # dlog("PUSH")
+        # else:
+        # dlog("NOPUSH")
+
+        # Make sure you aren't the next in a pawn chain that has to move.
+        # TODO: Make sure you're reinforcing the first pawn in a line WHILE not being taken
+        # TODO: Make sure if you aren't crucial reinforcement, PUSH PUSH PUSH
+        if check_space_wrapper(row + forward, col + 1, board_size) == team and check_space_wrapper(row + (forward * 2),
+                                                                                                   col + 2,
+                                                                                                   board_size) == opp_team:  # up and right
+            reinforce = True
+        elif check_space_wrapper(row + forward, col - 1, board_size) == team and check_space_wrapper(
+                row + (forward * 2), col - 2, board_size) == opp_team:  # up and left
+            reinforce = True
+        elif check_space_wrapper(row + forward, col + 1, board_size) == team and check_space_wrapper(
+                row + (forward * 2), col, board_size) == opp_team:
+            reinforce = True
+        elif check_space_wrapper(row + forward, col - 1, board_size) == team and check_space_wrapper(
+                row + (forward * 2), col, board_size) == opp_team:
+            reinforce = True
+
+        # if check_space_wrapper(row + (forward * 2), col + 1, board_size) == team and opponents == 0:
+        #     reinforce = False
+        # if check_space_wrapper(row + (forward * 2), col - 1, board_size) == team and opponents == 0:
+        #     reinforce = False
+        # Make sure you push to the end if possible
+        if row + forward == 0 or row + forward == board_size - 1:
+            reinforce = False
+
+        if check_space_wrapper(row + forward, col + 1, board_size) == opp_team:  # up and right
+            capture(row + forward, col + 1)
+            # dlog('Captured at: (' + str(row + forward) + ', ' + str(col + 1) + ')')
+
+        elif check_space_wrapper(row + forward, col - 1, board_size) == opp_team:  # up and left
+            capture(row + forward, col - 1)
+            # dlog('Captured at: (' + str(row + forward) + ', ' + str(col - 1) + ')')
+
+        # otherwise try to move forward
+        elif not (not (row + forward != -1) or not (row + forward != board_size) or check_space_wrapper(row + forward,
+                                                                                                        col,
+                                                                                                        board_size)) and not reinforce and push:
+            #               ^  not off the board    ^            and    ^ directly forward is empty
+            move_forward()
+            # dlog('Moved forward!')
+    else:
+        # Where do we want to spawn the pawns? Center > Edges since you cover two spaces
+        # Maybe check to see where the opponent spawned as black and then counter?
+        # If you're white, want to go down a lane without any of your pawns not next to one already populated, if populated
+        # then you want to just fill a row you already have
+        if team == Team.WHITE:
+            vert = 0
+            # Tracks the base of the opponent
+            opp = board_size - 1
+            white = True
+        else:
+            vert = board_size - 1
+            opp = 0
+            white = False
+
+        col_to_place = -1
+        while col_to_place < 0 or col_to_place >= board_size:
+            col_to_place = input("Which row do you want to spawn in? ")
+        
+        if 0 <= col_to_place <= board_size - 1 and not check_space(vert, col_to_place):
+            dlog("Chose: " + str(col_to_place))
+            spawn(vert, col_to_place)
+
+    bytecode = get_bytecode()
+    # dlog('Done! Bytecode left: ' + str(bytecode))
